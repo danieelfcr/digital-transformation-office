2a8558564f3fe7419db69e54232a47205f9b8a2a
danieelfcr <danielfcab2002@gmail.com>
2025-05-11 00:03:30 -0600

    First Commit - Creating initial structure

---
+++ MFSD-Solution/DataServices/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataServices.Models;
+
+namespace DataServices;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    
+}
---
+++ MFSD-Solution/DataServices/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataServices.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public bool IsAdmin { get; set; }
+    }
+}
