7fd5f5cc0572782874dbf35c5ff4a6df77eefe2f
danieelfcr <danielfcab2002@gmail.com>
2025-05-12 06:28:22 -0600

    feat: backend login

---
+++ MFSD-Solution/ApiService/DTOs/LoginRequest.cs
@@ -0,0 +1,16 @@
+﻿using ApiService.Validators;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.DTOs
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        [ValidEmailDomain("dto.com", ErrorMessage = "Solo se permiten correos con el dominio midominio.com.")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
