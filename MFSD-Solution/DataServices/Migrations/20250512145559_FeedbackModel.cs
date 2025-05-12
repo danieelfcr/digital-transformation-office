fa9b92c28418012df7a75062bb143dbf24a18e20
danieelfcr <danielfcab2002@gmail.com>
2025-05-12 08:57:14 -0600

    feat: migration

---
+++ MFSD-Solution/DataServices/Migrations/20250512145559_FeedbackModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataServices.Migrations
+{
+    /// <inheritdoc />
+    public partial class FeedbackModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feedback",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FeedbackDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedback", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Feedback_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$kLZxGJcSXhkD1MLHmrnxVuRz1Uya3LrR4cVL6deq7FxfqyDgkw5cC");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "$2a$11$Bmw6/jvvdSl0DkY6CAXke.AJb6Q5QBnNSWrTANx6f9XNKxRFKS1yi");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Password",
+                value: "$2a$11$4w62kqf0jHnHRYRHLAVG5OrAK.IUeYH7HZO2arJXrayVYCuZvPLgu");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedback_UserId",
+                table: "Feedback",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feedback");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$DJZlvLov3AwnbLAjGLw8juLeD33pPuzZJv6QEfxYTtsfTbPizjOKC");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "$2a$11$l/3XUN8wbJ3Mf5gUEJ1RBOz7hGdnVgyLXaw/6mRx0jlc31q29zGMW");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Password",
+                value: "$2a$11$Gw.p1L9xb31GastqIZuUzevpSTY4lmpFtB54yENHTY.Q26rxATIN6");
+        }
+    }
+}
